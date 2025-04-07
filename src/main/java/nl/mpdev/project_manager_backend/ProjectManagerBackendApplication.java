bc3fc807b142eaca5e60f3f6f69d7f2e023f8462
mjerta <info@mpdev.nl>
2025-04-07 22:55:42 +0200

    Initial commit

---
+++ src/main/java/nl/mpdev/project_manager_backend/ProjectManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package nl.mpdev.project_manager_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/mpdev/project_manager_backend/ProjectManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.mpdev.project_manager_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
