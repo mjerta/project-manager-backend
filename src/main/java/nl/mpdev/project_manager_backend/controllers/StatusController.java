77b8c64eab92ac95b3be0bec705e148dbd03d800
mjerta <info@mpdev.nl>
2025-04-21 23:03:57 +0200

    Add responsecode for deleteById endpoint

--- src/main/java/nl/mpdev/project_manager_backend/controllers/StatusController.java
+++ src/main/java/nl/mpdev/project_manager_backend/controllers/StatusController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import nl.mpdev.project_manager_backend.models.Status;
@@ -52,6 +53,7 @@ public class StatusController {
     return ResponseEntity.status(HttpStatus.OK).body(updatedStatus);
   }
 
+  @ResponseStatus(HttpStatus.NO_CONTENT)
   @DeleteMapping("/status/{id}")
   public void deleteStatus(@PathVariable Long id){
     statusService.deleteStatus(id);
