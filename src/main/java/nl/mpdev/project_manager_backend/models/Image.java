0f1185cebc5928a0710950b038b5a37d36deb899
mjerta <info@mpdev.nl>
2025-04-26 11:33:34 +0200

    Setup one-to-many relationship between Project and Image

--- src/main/java/nl/mpdev/project_manager_backend/models/Image.java
+++ src/main/java/nl/mpdev/project_manager_backend/models/Image.java
@@ -4,7 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -20,4 +22,7 @@ public class Image {
   private long size;
   @Lob
   private byte[] data;
+  @ManyToOne
+  @JoinColumn(name = "project_id", referencedColumnName = "id")
+  private Project project;
 }
--- src/main/java/nl/mpdev/project_manager_backend/models/Project.java
+++ src/main/java/nl/mpdev/project_manager_backend/models/Project.java
@@ -1,10 +1,16 @@
 package nl.mpdev.project_manager_backend.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -21,4 +27,6 @@ public class Project {
   @OneToOne
   @JoinColumn(name = "status_id", referencedColumnName = "id")
   private Status status;
+  @OneToMany(mappedBy = "project", cascade = { CascadeType.PERSIST, CascadeType.MERGE }, fetch = FetchType.EAGER)
+  private List<Image> images = new ArrayList<>();
 }
