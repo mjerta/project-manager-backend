17376a3e3006a87a9b911c4849a36d56a79645e5
mjerta <info@mpdev.nl>
2025-05-03 22:14:23 +0200

    Add the last part where i needed another mapper and covert it into another list

--- src/main/java/nl/mpdev/project_manager_backend/mappers/projects/ProjectsMapper.java
+++ src/main/java/nl/mpdev/project_manager_backend/mappers/projects/ProjectsMapper.java
@@ -5,14 +5,17 @@ import nl.mpdev.project_manager_backend.models.*;
 import nl.mpdev.project_manager_backend.dto.projects.request.ProjectCompleteRequestDto;
 import nl.mpdev.project_manager_backend.dto.projects.response.ProjectCompleteResponseDto;
 import nl.mpdev.project_manager_backend.services.*;
+import nl.mpdev.project_manager_backend.mappers.image.*;;
 
 @Component
 public class ProjectsMapper {
 
   private final StatusService statusService;
+  private final ImagesMapper imagesMapper;
 
-  public ProjectsMapper(StatusService statusService) {
+  public ProjectsMapper(StatusService statusService, ImagesMapper imagesMapper) {
     this.statusService = statusService;
+    this.imagesMapper = imagesMapper;
   }
 
   public Project toEntity(ProjectCompleteRequestDto requestDto) {
@@ -23,14 +26,18 @@ public class ProjectsMapper {
     return project;
   }
 
-
   public ProjectCompleteResponseDto toDto(Project entity) {
     ProjectCompleteResponseDto dto = new ProjectCompleteResponseDto();
     dto.setId(entity.getId());
     dto.setTitle(entity.getTitle());
     dto.setStatus(entity.getStatus().getName());
     dto.setDescription(entity.getDescription());
+    // probably needs some null checking here;
+    dto.setImageLinks(
+      entity.getImages().stream()
+        .map(image -> imagesMapper.toDto(image).getImageLink())
+        .toList()
+    );
     return dto;
   }
 }
-
