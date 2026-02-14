eb7c7c6371c5cf011cc00623a1e3c84a952be5b3
mjerta <info@mpdev.nl>
2026-02-14 22:27:18 +0100

    Update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO status (name, description) VALUES 
-('ti','Future task'),
+('too','Future task'),
 ('To Do', 'Task has not been started yet'),
 ('In Progress', 'Task is currently being worked on'),
 ('Done', 'Task has been completed');
