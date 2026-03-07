b800ee5580a410c52cef7b434077a68e67b57c9b
mjerta <info@mpdev.nl>
2026-03-07 22:31:10 +0100

    Add User and Authority entity for internal data base

---
+++ src/main/java/nl/mpdev/project_manager_backend/models/Authority.java
@@ -0,0 +1,31 @@
+package nl.mpdev.project_manager_backend.models;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "authorities")
+public class Authority implements GrantedAuthority {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String authority;
+
+}
