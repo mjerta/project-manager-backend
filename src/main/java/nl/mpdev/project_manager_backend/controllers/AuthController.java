c4037bffc9cbc78a8d74c7cc6a4430489771f8f5
mjerta <info@mpdev.nl>
2026-03-15 10:51:31 +0100

    Fix some imports

--- src/main/java/nl/mpdev/project_manager_backend/controllers/AuthController.java
+++ src/main/java/nl/mpdev/project_manager_backend/controllers/AuthController.java
@@ -1,13 +1,7 @@
 package nl.mpdev.project_manager_backend.controllers;
 
-import java.util.Map;
-
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class AuthController {
