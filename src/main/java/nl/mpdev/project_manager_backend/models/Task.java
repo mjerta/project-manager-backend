c5310e386a8aca57191284bc62c6937db56d0de9
mjerta <info@mpdev.nl>
2026-03-29 12:28:12 +0200

    Added new entity

---
+++ src/main/java/nl/mpdev/project_manager_backend/models/Task.java
@@ -0,0 +1,33 @@
+package nl.mpdev.project_manager_backend.models;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "task")
+public class Task {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private String description;
+  @ManyToOne
+  @JoinColumn(name = "project_id", referencedColumnName = "id")
+  private Project project;
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+  @JoinColumn(name = "status_id", referencedColumnName = "id")
+  private Status status;
+
+}
