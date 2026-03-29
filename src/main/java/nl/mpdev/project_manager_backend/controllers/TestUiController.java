183d9788bb9b916d27604936a6cec83972eb4684
mjerta <info@mpdev.nl>
2026-03-29 23:37:41 +0200

    Update the test ui

--- src/main/java/nl/mpdev/project_manager_backend/controllers/TestUiController.java
+++ src/main/java/nl/mpdev/project_manager_backend/controllers/TestUiController.java
@@ -1,6 +1,5 @@
 package nl.mpdev.project_manager_backend.controllers;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,15 +10,9 @@ import nl.mpdev.project_manager_backend.config.TestUiLoginConstants;
 @Controller
 public class TestUiController {
 
-  private String adminEmail;
-
   @GetMapping("/test-ui")
   public String testUi(Model model) {
     model.addAttribute("tokenStorageKey", TestUiLoginConstants.TEST_UI_STORAGE_KEY);
-    model.addAttribute("adminEndpoint", "/api/v1/admin");
-    model.addAttribute("statusEndpoint", "/api/v1/status");
-    model.addAttribute("firstTestEndpoint", "/api/v1/first-test");
-    model.addAttribute("adminEmail", adminEmail);
     return "test-console";
   }
 
