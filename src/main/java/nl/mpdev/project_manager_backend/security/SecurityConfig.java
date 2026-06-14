f619243eab8a6570218d2eee864f213bcba60d13
Maarten Postma <mpostma@magicsoftware.com>
2026-06-14 17:11:12 +0200

    Allow all cross origin

--- src/main/java/nl/mpdev/project_manager_backend/security/SecurityConfig.java
+++ src/main/java/nl/mpdev/project_manager_backend/security/SecurityConfig.java
@@ -134,10 +134,11 @@ public class SecurityConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:8080"));
+    configuration.setAllowedOriginPatterns(List.of("*"));
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept", "Origin"));
     configuration.setAllowCredentials(true);
+
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", configuration);
     return source;
