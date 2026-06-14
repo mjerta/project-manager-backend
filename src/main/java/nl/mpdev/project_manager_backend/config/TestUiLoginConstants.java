f4d1e3bae2eefb5fbb6ce3a99e05e7b66cc3dedf
Maarten Postma <mpostma@magicsoftware.com>
2026-06-14 18:39:32 +0200

    refactor: remove legacy test ui flow

--- src/main/java/nl/mpdev/project_manager_backend/config/TestUiLoginConstants.java
+++
@@ -1,9 +0,0 @@
-package nl.mpdev.project_manager_backend.config;
-
-public final class TestUiLoginConstants {
-  public static final String TEST_UI_LOGIN_ATTR = "TEST_UI_LOGIN";
-  public static final String TEST_UI_STORAGE_KEY = "pm_jwt_token";
-
-  private TestUiLoginConstants() {
-  }
-}
